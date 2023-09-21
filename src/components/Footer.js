c4d8727edf5dd72a46e23717c4193f58c9c22fe6
Graham <blundell.gp@gmail.com>
2023-09-21 13:07:19 -0700

    added basic style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Footer.css'
+import rgb from '../assets/stack-rgb.png'
 
 const Footer = () => {
     return(
         <>
         <div>
             <p>2023 Quizard</p>
+            <p>Brought to you by </p><img src={rgb} />
         </div>
         </>
     )
