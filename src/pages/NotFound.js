da10e7d48f8c68745912e2b488fea4fba03edc50
Graham <blundell.gp@gmail.com>
2023-09-21 10:36:06 -0700

    added home page and not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,6 +1,14 @@
 import React from "react";
 import './NotFound.css'
+import snek from '../assets/snek-404.png'
 
 
+const NotFound = () => {
+    return (
+        <div>
+            <img src='snek' />
+        </div>
+    )
+}
 
 export default NotFound
