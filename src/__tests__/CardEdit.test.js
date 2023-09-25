a71a92e1672861c457fc7ed1fed6e83eb8909e6f
raquel king <kingraq1007@gmail.com>
2023-09-25 12:43:36 -0500

    completed card edit functionality and test

---
+++ src/__tests__/CardEdit.test.js
@@ -0,0 +1,37 @@
+import { render, screen } from '@testing-library/react';
+import CardEdit from '../pages/CardEdit';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import mockCards from "../mockCards";
+  
+const renderEdit = () => {
+    render(
+      <MemoryRouter initialEntries={["/cardedit/1"]}>
+        <Routes>
+          <Route path="/cardedit/:id" element={<CardEdit cards={mockCards} />} />
+        </Routes>
+      </MemoryRouter>
+    )
+  }
+  
+  describe('<CardEdit />', () => {
+    it("renders the CardEdit page for the user", () => {
+      renderEdit()
+      const editLink = screen.getByRole('link', {
+        name: /edit card/i
+      })
+      expect(editLink).toBeInTheDocument()
+  
+      const submitButton = screen.getByRole('button', { name: /submit/i })
+      expect(submitButton).toBeInTheDocument()
+    })
+  
+    it("has a form with entries for question and answer", () => {
+      renderEdit()
+  
+      const formQuestion = screen.getByText("Question")
+      expect(formQuestion.getAttribute("for")).toEqual("Question")
+  
+      const formAnswer = screen.getByText("Answer")
+      expect(formAnswer.getAttribute("for")).toEqual("answer")
+    })
+  })
