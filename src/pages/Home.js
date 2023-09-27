265ca21ef92bd28fad3391c0e4947fda784d9ecb
Bao Khanh Tran <ktbv.22@gmail.com>
2023-09-27 10:49:15 -0700

    bao's styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { gsap } from 'gsap'
 import './Home.css'
 import wizR from '../assets/WizLizRGB.png'
+
+
 const Home = () => {
-return (
-     <div className='home-container'>
-       <div className='header'>
-         <h1>QUIZARD</h1>
-       </div>
-        <img src={wizR} className='homeImg' alt='homeImg' />
+  const parent = useRef()
+  const headerRef = useRef()
+  const tl = useRef()
+  const h1Ref = useRef()
+  const h2Ref = useRef()
+  const navigate = useNavigate()
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      tl.current = gsap.timeline({defaults: {duration : 1}})
+      .from(h1Ref.current, { y: "-100%", ease: 'bounce', rotation: "-180"})
+      .from(h2Ref.current, { y: "+100vh", duration: 1, ease: "back.out(1.3)"})
+      .from('.homeImg', { opacity: 0, duration: 1.5}, .5)
+    }, parent)
+    return () => ctx.revert()
+  }, [])
+  
+  return (
+    <div className='home-container' ref={parent}>
+      <div className='header' ref={headerRef}>
+        <h1 ref={h1Ref}>QUIZARD</h1>
+        <h2 ref={h2Ref}>Master Any Topic, One Card at a Time!</h2>
+      </div>
+      <img src={wizR} className='homeImg' alt='homeImg' />
+      <div className='home-btns'>
+        <button onClick={() => navigate("/login")}>Log In!</button>
+        <button onClick={() => navigate("/signup")}>Sign Up!</button>
+      </div>
     </div>
-    )
+  )
 }
 
 export default Home
