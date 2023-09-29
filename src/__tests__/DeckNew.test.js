0f25b584b6168ca91b150bb898c74f2ebe9d2904
Graham <blundell.gp@gmail.com>
2023-09-28 21:14:48 -0700

    test code cvg

--- src/__tests__/DeckNew.test.js
+++ src/__tests__/DeckNew.test.js
@@ -4,17 +4,51 @@ import { BrowserRouter } from 'react-router-dom'
 import mockUsers from '../mockUsers.js'
 
 describe("<DeckNew />", () => {
-    beforeEach(() => {
-      render(
-        <BrowserRouter>
-          <DeckNew currentUser={mockUsers[0]}/>
-        </BrowserRouter>
-      )
+  it("renders the Deck New page", () => {
+    render(
+      <BrowserRouter>
+        <DeckNew currentUser={mockUsers[0]} />
+      </BrowserRouter>
+    )
+    const element = screen.getByText("Add A New Deck")
+    expect(element).toBeInTheDocument()
+  })
+
+  it("renders an image", () => {
+    render(
+      <BrowserRouter>
+        <DeckNew currentUser={mockUsers[0]} />
+      </BrowserRouter>
+    )
+    const img = screen.getByRole('img')
+    expect(img).toBeInTheDocument()
+  })
+
+  it("renders a text box", () => {
+    render(
+      <BrowserRouter>
+        <DeckNew currentUser={mockUsers[0]} />
+      </BrowserRouter>
+    )
+    const txtBox = screen.getByRole('textbox', {
+      name: /title of deck/i
     })
-  
-    it("renders the Deck New page", () => {
-      const element = screen.getByText("Add A New Deck")
-      expect(element).toBeInTheDocument()
+    expect(txtBox).toBeInTheDocument()
+  })
+
+  it("renders a button", () => {
+    render(
+      <BrowserRouter>
+        <DeckNew currentUser={mockUsers[0]} />
+      </BrowserRouter>
+    )
+    const btn = screen.getByRole('button', {
+      name: /create!/i
     })
+    expect(btn).toBeInTheDocument()
+  })
 
 })
+
+
+
--- src/__tests__/SignUp.test.js
+++ src/__tests__/SignUp.test.js
@@ -3,14 +3,58 @@ import { BrowserRouter } from "react-router-dom"
 import SignUp from "../pages/SignUp"
 
 describe("<SignUp />", () => {
-    it("Shows the page with no errors", () => {
-      render(
-        <BrowserRouter>
-          <SignUp />
-        </BrowserRouter>
-      )
-      const greeter = screen.getByText(/sign up/i)
-      expect(greeter).toBeInTheDocument()
+  it("Shows the page with no errors", () => {
+    render(
+      <BrowserRouter>
+        <SignUp />
+      </BrowserRouter>
+    )
+    const greeter = screen.getByText(/sign up/i)
+  })
+
+  it("Shows an image", () => {
+    render(
+      <BrowserRouter>
+        <SignUp />
+      </BrowserRouter>
+    )
+    const img = screen.getByRole('img', {
+      name: /signup\-liz/i
     })
-     
-  });
+    expect(img).toBeVisible()
+  })
+
+  it("has an input field", () => {
+    render(
+      <BrowserRouter>
+        <SignUp />
+      </BrowserRouter>
+    )
+    const email = screen.getByRole('textbox')
+    expect(email).toBeInTheDocument()
+    expect(email).toHaveValue("")
+  })
+
+  it("has placeholder text in the input", () => {
+    render(
+      <BrowserRouter>
+        <SignUp />
+      </BrowserRouter>
+    )
+    const pw = screen.getByPlaceholderText(/confirm password/i)
+    expect(pw).toBeInTheDocument()
+  })
+
+  it("Submits the form", () => {
+    render(
+      <BrowserRouter>
+        <SignUp />
+      </BrowserRouter>
+    )
+    const submit = screen.getByRole('button', {
+      name: /submit/i
+    })
+    expect(submit).toBeInTheDocument()
+  })
+
+});
